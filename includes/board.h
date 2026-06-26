3b31eeb217328b60b09fb9ecbcce2fea1cd5a0d5
Its-Suraj <20244098@g.deu.ac.kr>
2026-06-26 22:37:02 +0900

    Create board.h

---
+++ includes/board.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Board {
+private:
+    int game_rows;
+    vector<vector<int>> grid;
+
+public:
+    Board(int size);
+
+    void Update_Board(const vector<pair<int,int>>& Player_1, const vector<pair<int,int>>& Player_2);
+
+    void Display_Board();
+};
+#endif
